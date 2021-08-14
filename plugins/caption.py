eaaccf46ec8301516045e99b6c49aa2c5162177b
jack4279 <88567670+jack4279@users.noreply.github.com>
2021-08-14 21:55:42 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -15,15 +15,6 @@ async def caption(client, message: Message):
     FILE_NAME = bool(os.environ.get('FILE_NAME', False))
     if FILE_NAME and not button_name:
         media = message.audio or message.video or message.document or message.animation
-        if (media is not None) and (media.file_name is not None):
-            file = media.file_name
-            _file = file.replace("-", " ").replace("_", " ").replace(".mp4", " ").replace(".mkv", " ").replace(".pdf", " ").replace(".apk", " ").replace(".mp3", " ").replace(".zip", " ")
-            caption = CAPTION
-            await message.edit(
-                f"{_file}\n\n"
-                f"{caption}")
-        else:
-            await message.edit(CAPTION)
     if FILE_NAME and button_name: 
         media = message.audio or message.video or message.document or message.animation
         if (media is not None) and (media.file_name is not None):
