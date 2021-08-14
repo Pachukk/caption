06771c6ad8e4f0073449321b7667fa77df454734
jack4279 <88567670+jack4279@users.noreply.github.com>
2021-08-14 22:35:27 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -5,6 +5,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait
 
+CAPTION=Config.CAPTION
 
 @Client.on_message(filters.channel & filters.media)
 async def caption(client, message: Message):
